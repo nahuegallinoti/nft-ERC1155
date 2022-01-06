783a6d66cfcc6b7bccdda31efe055ff0aaf78718
nahuegallinoti <nahuegallinoti@gmail.com>
2022-01-06 16:40:48 -0300

    Update MyCollectibles.sol

--- MyCollectibles.sol
+++ MyCollectibles.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract MyCollectibles is ERC1155 {
     constructor() ERC1155("https://raw.githubusercontent.com/nahuegallinoti/static-nft/main/000000000000000000000000000000000000000000000000000000000000000{id}.json") {
-        _mint(msg.sender, 1 /* Id Zanahoria */, 10 /* Cantidad */, "");
+        _mint(msg.sender, 1 /* Id Nagu */, 10 /* Cantidad */, "");
     }
 }
